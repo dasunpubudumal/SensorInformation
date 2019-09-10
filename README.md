836b749cb1d9b259cbbfb5cac080d63154203210
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2019-09-10 06:42:25 +0530

    accelometer

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Sensor Reading from Android
 
+## Sensors available in Huawei nova 3i
+
+* accelerometer-lsm6ds3-c
+* mag-akm09918
+* orientation
+* light-ltr578
+* proximity-ltr578
+* st-lsm6ds3
+* gravity
+* linear Acceleration
+* rotation Vector
+* HALL sensor
+* uncalibrated Magnetic Field
+* game Rotation Vector
+* uncalibrated Gyroscope
+* significant Motion
+* step counter
+* geomagnetic Rotation Vector
+* phonecall sensor
+* RPC sensor
+
 ## References
 
 1. [Android Official Documentation on Sensors](https://developer.android.com/guide/topics/sensors/sensors_overview#java)
--- app/src/main/java/com/realitix/sensorinformation/MainActivity.java
+++ app/src/main/java/com/realitix/sensorinformation/MainActivity.java
@@ -9,28 +9,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
-    private Sensor mGravity;
-    private float gravityValue;
-    TextView textView;
+    private Sensor mGravity, mAccelometer;
+    private float gravityValue, accelometerValue;
+    private TextView txtMGravity;
+    private TextView txtMAccelometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-//        sensors.forEach((sensor) -> System.out.println(sensor.getName()));
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensors.forEach((sensor) -> System.out.println(sensor.getName()));
         mGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        mAccelometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.gravity);
+        txtMGravity = (TextView) findViewById(R.id.gravity);
+        txtMAccelometer = (TextView) findViewById(R.id.accelometer);
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        gravityValue = sensorEvent.values[0];
-        textView.setText(String.valueOf(gravityValue));
+        switch (sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_GRAVITY:
+                gravityValue = sensorEvent.values[0];
+                txtMGravity.setText(String.valueOf(gravityValue));
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                accelometerValue = sensorEvent.values[0];
+                txtMAccelometer.setText(String.valueOf(accelometerValue));
+                break;
+            default:
+                gravityValue = 0.0f;
+                txtMGravity.setText(R.string.nan);
+                txtMAccelometer.setText(R.string.nan);
+        }
     }
 
     @Override
@@ -40,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, mAccelometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
