ece8be6755e1cd1b40dd8dbcf3ea0afb65acd3e2
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2019-09-10 06:47:26 +0530

    light

--- app/src/main/java/com/realitix/sensorinformation/MainActivity.java
+++ app/src/main/java/com/realitix/sensorinformation/MainActivity.java
@@ -14,10 +14,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
-    private Sensor mGravity, mAccelometer;
-    private float gravityValue, accelometerValue;
-    private TextView txtMGravity;
-    private TextView txtMAccelometer;
+    private Sensor mGravity, mAccelometer, mLight;
+    private float gravityValue, accelometerValue, lightValue;
+    private TextView txtMGravity, txtMAccelometer, txtMLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +26,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensors.forEach((sensor) -> System.out.println(sensor.getName()));
         mGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         mAccelometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
         setContentView(R.layout.activity_main);
         txtMGravity = (TextView) findViewById(R.id.gravity);
         txtMAccelometer = (TextView) findViewById(R.id.accelometer);
-
+        txtMLight = (TextView) findViewById(R.id.light);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        float value = sensorEvent.values[0];
         switch (sensorEvent.sensor.getType()) {
             case Sensor.TYPE_GRAVITY:
-                gravityValue = sensorEvent.values[0];
+                gravityValue = value;
                 txtMGravity.setText(String.valueOf(gravityValue));
                 break;
             case Sensor.TYPE_ACCELEROMETER:
-                accelometerValue = sensorEvent.values[0];
+                accelometerValue =value;
                 txtMAccelometer.setText(String.valueOf(accelometerValue));
                 break;
+            case Sensor.TYPE_LIGHT:
+                lightValue = value;
+                txtMLight.setText(String.valueOf(lightValue));
             default:
                 gravityValue = 0.0f;
                 txtMGravity.setText(R.string.nan);
@@ -59,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         sensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, mAccelometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
